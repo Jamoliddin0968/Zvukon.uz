43ddb731e51ef3e8821a95bbf57325d983003336
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-02-16 20:58:09 +0500

    upg

--- shop/models.py
+++ shop/models.py
@@ -1,17 +1,18 @@
-from django.db import models
-from django.conf import settings
 import os
 from uuid import uuid4
-from django.utils.html import format_html
-from django.utils.translation import gettext_lazy as _
-from PIL import Image
+
 import PIL
-from imagekit.models import ImageSpecField
-from pilkit.processors import *
+from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core.validators import FileExtensionValidator
+from django.db import models
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
+from imagekit.models import ImageSpecField
+from PIL import Image
+from pilkit.processors import *
 
-VIDEO_EXTENSIONS = ['mp4', '.avi', '.wmv', '.mov']
+VIDEO_EXTENSIONS = ['mp4']
 MAX_VIDEO_SIZE = 104857600  # 100 MB
 
 
@@ -25,105 +26,125 @@ def rename_product_image(instance, filename):
     return os.path.join(upload_to, filename)
 
 
-    
-    
 class Category(models.Model):
     name = models.CharField(max_length=255, verbose_name=_(
         "Kategoriya nomi"), null=True, blank=True)
     description = models.TextField(verbose_name=_(
         "Kategoriya tarifi"), null=True, blank=True)
-    image = models.ImageField(upload_to="category-image",verbose_name=_("Rasm"))
+    image = models.ImageField(
+        upload_to="category-image", verbose_name=_("Rasm"))
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     video_file = models.FileField(upload_to='videos/', validators=[
         FileExtensionValidator(allowed_extensions=VIDEO_EXTENSIONS),
-    ],null=True,blank=True)
-    
-        
+    ], null=True, blank=True)
+
     image_fon = ImageSpecField(
         source='image',
         processors=[Resize(950, 700)],
-        format='PNG',  
+        format='PNG',
         options={'quality': 100}
     )
+
     def __str__(self) -> str:
         return self.name
 
     class Meta:
-        #db_table = "Kategoriya"
+        # db_table = "Kategoriya"
         verbose_name = _("Kategoriya")
         verbose_name_plural = _("Kategoriyalar")
 
+
 class SubCategory(models.Model):
-    name = models.CharField(max_length=255,null=True,blank=True,verbose_name=_("Subkategoriya nomi"))
+    name = models.CharField(max_length=255, null=True,
+                            blank=True, verbose_name=_("Subkategoriya nomi"))
     image = models.ImageField(upload_to="subcategory-images")
-    category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True,blank=True,verbose_name=_("Kategoriya"))
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True, verbose_name=_("Kategoriya"))
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self) -> str:
         return self.name
-    
+
     class Meta:
         verbose_name = _("SubKategoriya")
         verbose_name_plural = _("SubKategoriyalar")
 
 
+SOM, DOLLAR = ("so'm", "$")
+
+
 class Product(models.Model):
+
+    VALYUTA = (
+        (SOM, "So'm"), (DOLLAR, "Dollar")
+    )
     name = models.CharField(max_length=255, verbose_name=_("nomi"))
-    price = models.FloatField(default=0, verbose_name=_("narxi"))
+    value = models.FloatField(default=0, verbose_name=_("narxi"))
+    currency = models.CharField(
+        choices=VALYUTA, default=SOM, max_length=7, verbose_name=_("Valyutasi"))
     subcategory = models.ForeignKey(
         SubCategory, on_delete=models.SET_NULL, null=True, blank=True, verbose_name=_("Kategoriya"))
     description = models.TextField(verbose_name=_("Ta'rifi"))
-    characteristic = models.TextField(verbose_name=_("Xarakteristikasi"),null=True)
-    
-    
+    characteristic = models.TextField(
+        verbose_name=_("Xarakteristikasi"), null=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.name
 
-    class Meta:
-        verbose_name = _("Maxsulot")
-        verbose_name_plural = _("Maxsulotlar")
     @property
     def small_image(self):
-        return  self.image_set.first().image_small.url
-    
+        return self.image_set.first().image_small.url
+
     @property
     def medium_images(self):
-        lst = [ i.image_medium.url for i in self.image_set.all()[:4]]
+        lst = [i.image_medium.url for i in self.image_set.all()[:4]]
         while len(lst) < 4:
             lst.append(None)
         return lst
 
+    @property
+    def price(self):
+        if self.currency == DOLLAR:
+            return DOLLAR +" " + str(self.value)
+        return str(self.value) + " " + SOM
 
+    @property
+    def categoryname(self):
+        return self.subcategory.name
+
+    class Meta:
+        verbose_name = _("Maxsulot")
+        verbose_name_plural = _("Maxsulotlar")
 class Image(models.Model):
     image = models.ImageField(
-        upload_to=rename_product_image, verbose_name=_("rasm"), max_length=150,help_text=_("Tavsiya etiladiogan rasm o'lchani 719x791"))
+        upload_to=rename_product_image, verbose_name=_("rasm"), max_length=150, help_text=_("Tavsiya etiladiogan rasm o'lchani 719x791"))
     product = models.ForeignKey(
         'Product', on_delete=models.CASCADE, verbose_name=_("Maxsulot"))
 
     image_small = ImageSpecField(
         source='image',
         processors=[Resize(719, 791)],
-        format='PNG',  
+        format='PNG',
         options={'quality': 100}
     )
     image_medium = ImageSpecField(
         source='image',
-        processors=[Resize(774,800)],
+        processors=[Resize(774, 800)],
         format='JPEG',
         options={'quality': 100}
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self) -> str:
         return self.product.name+' ' + self.image.url
-    
+
     class Meta:
         verbose_name = _("Rasm")
         verbose_name_plural = _("Rasmlar")
